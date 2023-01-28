bb4368da11167dd30d87df1566cb7b08db495383
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-01-28 13:35:58 +0530

    LC

---
+++ LC/ClassesMoreThan5Students.sql
@@ -0,0 +1,20 @@
+/*
+Table: Courses
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student     | varchar |
+| class       | varchar |
++-------------+---------+
+(student, class) is the primary key column for this table.
+Each row of this table indicates the name of a student and the class in which they are enrolled.
+
+Write an SQL query to report all the classes that have at least five students.
+Return the result table in any order.
+*/
+
+# Write your MySQL query statement below
+SELECT class 
+FROM Courses
+GROUP BY class
+HAVING COUNT(class)>=5
