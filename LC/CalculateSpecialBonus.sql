ee23f8e75f509d94716900a1f366082d71d0953c
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-03-26 17:18:20 +0530

    LC

--- LC/CalculateSpecialBonus.sql
+++ LC/CalculateSpecialBonus.sql
@@ -1,1 +1,21 @@
 
+
+/*
+SIMPLE BIT LOGIC
+*/
+
+SELECT employee_id , salary * ( employee_id%2 ) * ( name not like 'M%') as bonus
+FROM employees
+ORDER BY employee_id;
+
+
+/*
+CASE STATEMENT 
+*/
+
+SELECT employee_id,
+case when employee_id%2=1 AND name NOT LIKE 'M%' then salary
+ELSE 0
+END AS bonus 
+FROM Employees
+ORDER BY employee_id
