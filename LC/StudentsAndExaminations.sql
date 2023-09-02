d1b1a7cbe85d0988e1cff412f5aa1cd220ac57d6
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2023-09-03 04:15:30 +0530

    LC

--- LC/StudentsAndExaminations.sql
+++ LC/StudentsAndExaminations.sql
@@ -104,3 +104,10 @@ Alex did not attend any exams.
 John attended the Math exam 1 time, the Physics exam 1 time, and the Programming exam 1 time.
 */
 
+SELECT St.student_id, St.student_name, Sub.subject_name, COUNT(E.subject_name) AS attended_exams
+FROM Students St JOIN Subjects Sub
+LEFT JOIN Examinations E
+ON St.student_id=E.student_id AND Sub.subject_name=E.subject_name
+GROUP BY St.student_id, Sub.subject_name
+ORDER BY student_id, subject_name
+
